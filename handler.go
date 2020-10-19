755364a129fdcc2dda86fca502faacaaedf0e80b
Yota Hamada <yohamta@gmail.com>
2020-10-19 22:58:30 +0900

    fix interface

--- handler.go
+++ handler.go
@@ -2,15 +2,11 @@ package furex
 
 // TouchHandler represents a touch handler
 type TouchHandler interface {
-	Component
-
 	HandleJustPressedTouchID(touchID int) bool
 	HandleJustReleasedTouchID(touchID int)
 }
 
 // MouseHandler represents a mouse handler
 type MouseHandler interface {
-	Component
-
 	HandleMouse(x, y int) bool
 }
